390ff537f0f41c9b1992815283122e260da48e41
Saeed Physics <sdmi13790@gmail.com>
2022-10-21 14:21:19 +0330

    fiding root in matlab

--- jalase 2/root.m
+++ jalase 2/root.m
@@ -5,11 +5,26 @@ syms x
 f = input('f(x)= ');
 x1 = input('x1= ');
 x2 = input('x2= ');
+e = input('e= ');
 f = inline(f);
 
 if f(x1) * f(x2) > 0
     error('change the interval')
 end
 
-n=0
-xn=(x1+x2)/2
+n = 0;
+xn = (x1 + x2) / 2;
+
+while abs(f(xn)) > e
+    n = n + 1;
+
+    if f(x1) * f(xn) > e
+        x2 = xn;
+    else
+        x1 = xn;
+    end
+
+    xn = (x1 + x2) / 2;
+end
+
+display('The root is xn = ', xn)
