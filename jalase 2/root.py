f2205a48dbbc74d8697e465f359883d25a8edc4e
Saeed Physics <sdmi13790@gmail.com>
2022-10-28 13:01:08 +0330

    find root in python

--- jalase 2/root.py
+++ jalase 2/root.py
@@ -5,19 +5,20 @@ def f(x):
     return x**3-x**2+2
 
 
-x1 = 1
-x2 = -10
+x1 = -200
+x2 = 300
+
 
 if f(x1)*f(x2) >= 0:
     sys.exit("please change the interval")
-elif (x1-x2) >= 0.01:
-    xn = (x1+x2)/2
 
-if f(xn) == 0:
-    pass
-elif f(x1)*f(xn) < 0:
-    x2 = xn
-else:
-    x1 = xn
+while (x2-x1) >= 0.01:
+    xn = (x1+x2)/2
+    if f(xn) == 0:
+        break
+    elif f(x1)*f(xn) < 0:
+        x2 = xn
+    else:
+        x1 = xn
 
 print("the root is: ", "%4f" % xn)
